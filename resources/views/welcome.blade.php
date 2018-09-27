e2afb455463bf3c7d75949b0ba5030d27763cf52
acsotech <acsotech.mm@gmail.com>
2018-09-27 13:48:15 +0630

    home link adding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                 
                 <h1>
 
-                   Sai yan naing to Master
+                   Sai yan naing to Master editing by kyawsoenaing
                 </h1>
                 <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
                 <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
