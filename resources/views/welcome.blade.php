5726b950fd252ea7c71894564ce48321f80c1fa9
Acso Tech <acsotech.mm@gmail.com>
2018-09-27 12:46:06 +0630

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,11 +53,7 @@
             <div class="container-fluid">
                 
                 <h1>
-<<<<<<< HEAD
                     Sai Yan Naing master
-=======
-                    Sai Yan Naing second path
->>>>>>> saiyannaing
                 </h1>
                 <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
                 <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
