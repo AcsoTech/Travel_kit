bd1a5fc6f649f1c439bdab7e0e80b82a41825d5a
Acso Tech <acsotech.mm@gmail.com>
2018-09-27 12:48:50 +0630

    change second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
             <div class="container-fluid">
                 
                 <h1>
-                    Sai Yan Naing second path
+                    Sai Yan Naing second place
                 </h1>
                 <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
                 <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
