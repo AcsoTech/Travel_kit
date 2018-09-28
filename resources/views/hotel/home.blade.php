afcf4a515356f01a1bf8fd7a1cc800b0c6bbbb2f
AcsoTech <acsotech.mm@gmail.com>
2018-09-29 00:21:40 +0630

    Add Reviews

--- resources/views/hotel/home.blade.php
+++ resources/views/hotel/home.blade.php
@@ -36,69 +36,72 @@
 
 @section('content')
     <div class="row mt-3">
-       <div class="card col" style="width: 28rem;">
-       <a href="{{ url('/room') }}" class="logo-text">
+        <div class="card col" style="width: 28rem;">
             <div class="row mb-1">
-                <img class="card-img-top col-6 pt-1" src="{{asset('img/hotel/hotel1.jpg')}}" alt="Card image cap">
+                <img class="card-img-top card-hotel-image col-6 pt-1" src="{{asset('img/hotel/hotel1.jpg')}}" alt="Card image cap">
                 <div class="card-body col-6 p-1">
-                    <p class="text-primary card-title">Bangan King Hotel <i class="fa fa-thumbs-up text-success"></i> </p>
-                    <p>
-                        <i class="fa fa-star text-warning"></i>
-                        <i class="fa fa-star text-warning"></i>
-                        <i class="fa fa-star text-warning"></i>
-                    <p>
-                    <p class="card-text">Excellent 8.8</hp>
-                    <p class="text-success"><i class="fa fa-wifi"> </i>  Free Wi-fi</p>
-                    <p class="text-success"><i class="fa fa-check"> Pay at the hotel</i>
-                    <p class="text-primary"><i class="fa fa-dollar"></i> 51/night</p>
+                    <p class="text-primary card-title">Bangan King Hotel &nbsp;
+                        3 <i class="fa fa-star text-warning"></i>
+                    </p>
+                    <a href="" class="location-link">
+                        <i class="fa fa-location-arrow"></i>
+                        No (15/B), 26 Street, Between 76 Street and 75 Street, Chan Aye 
+                        Tharsan Township, Central Mandalay, Mandalay, Myanmar - View on map agoda
+                    </a>
+                    <p class="text-dark mt-2"><i class="fa fa-flag-o"></i>&nbsp; Excellent 8.8</p>
+                    <p class="text-success"><i class="fa fa-wifi"></i>&nbsp; Free Wi-fi</p>
+                    <p class="text-success"><i class="fa fa-cutlery"></i>&nbsp; Free Breafast</p>
+                    <p class="text-success"><i class="fa fa-check"></i>&nbsp; Pay at the hotel</p>
+                    <a href="" class="float-right mr-5">Reviews &nbsp;<i class="fa fa-angle-right"></i></a>
                 </div>
             </div>
-            </a>
         </div>
-      
+       
     </div>
-
      <div class="row mt-3">
         <div class="card col" style="width: 28rem;">
             <div class="row mb-1">
-                <img class="card-img-top col-6 pt-1" src="{{asset('img/hotel/hotel2.jpg')}}" alt="Card image cap">
+                <img class="card-img-top card-hotel-image col-6 pt-1" src="{{asset('img/hotel/hotel1.jpg')}}" alt="Card image cap">
                 <div class="card-body col-6 p-1">
-                    <p class="text-primary card-title">Bangan King Hotel <i class="fa fa-thumbs-up text-success"></i> </p>
-                    <p>
-                        <i class="fa fa-star text-warning"></i>
-                        <i class="fa fa-star text-warning"></i>
-                        <i class="fa fa-star text-warning"></i>
-                    <p>
-                    <p class="card-text">Excellent 8.8</hp>
-                    <p class="text-success"><i class="fa fa-wifi"> </i>  Free Wi-fi</p>
-                    <p class="text-success"><i class="fa fa-check"> Pay at the hotel</i>
-                    <p class="text-primary"><i class="fa fa-dollar"></i> 51/night</p>
+                    <p class="text-primary card-title">Bangan King Hotel &nbsp;
+                        3 <i class="fa fa-star text-warning"></i>
+                    </p>
+                    <a href="" class="location-link">
+                        <i class="fa fa-location-arrow"></i>
+                        No (15/B), 26 Street, Between 76 Street and 75 Street, Chan Aye 
+                        Tharsan Township, Central Mandalay, Mandalay, Myanmar - View on map agoda
+                    </a>
+                    <p class="text-dark mt-2"><i class="fa fa-flag-o"></i>&nbsp; Excellent 8.8</p>
+                    <p class="text-success"><i class="fa fa-wifi"></i>&nbsp; Free Wi-fi</p>
+                    <p class="text-success"><i class="fa fa-cutlery"></i>&nbsp; Free Breafast</p>
+                    <p class="text-success"><i class="fa fa-check"></i>&nbsp; Pay at the hotel</p>
+                    <a href="" class="float-right mr-5">Reviews &nbsp;<i class="fa fa-angle-right"></i></a>
                 </div>
             </div>
-    
         </div>
+       
     </div>
-
      <div class="row mt-3">
         <div class="card col" style="width: 28rem;">
             <div class="row mb-1">
-                <img class="card-img-top col-6 pt-1" src="{{asset('img/hotel/hotel3.jpg')}}" alt="Card image cap">
+                <img class="card-img-top card-hotel-image col-6 pt-1" src="{{asset('img/hotel/hotel1.jpg')}}" alt="Card image cap">
                 <div class="card-body col-6 p-1">
-                    <p class="text-primary card-title">Bangan King Hotel <i class="fa fa-thumbs-up text-success"></i> </p>
-                    <p>
-                        <i class="fa fa-star text-warning"></i>
-                        <i class="fa fa-star text-warning"></i>
-                        <i class="fa fa-star text-warning"></i>
-                    <p>
-                    <p class="card-text">Excellent 8.8</hp>
-                    <p class="text-success"><i class="fa fa-wifi"> </i>  Free Wi-fi</p>
-                    <p class="text-success"><i class="fa fa-check"> Pay at the hotel</i>
-                    <p class="text-primary"><i class="fa fa-dollar"></i> 51/night</p>
+                    <p class="text-primary card-title">Bangan King Hotel &nbsp;
+                        3 <i class="fa fa-star text-warning"></i>
+                    </p>
+                    <a href="" class="location-link">
+                        <i class="fa fa-location-arrow"></i>
+                        No (15/B), 26 Street, Between 76 Street and 75 Street, Chan Aye 
+                        Tharsan Township, Central Mandalay, Mandalay, Myanmar - View on map agoda
+                    </a>
+                    <p class="text-dark mt-2"><i class="fa fa-flag-o"></i>&nbsp; Excellent 8.8</p>
+                    <p class="text-success"><i class="fa fa-wifi"></i>&nbsp; Free Wi-fi</p>
+                    <p class="text-success"><i class="fa fa-cutlery"></i>&nbsp; Free Breafast</p>
+                    <p class="text-success"><i class="fa fa-check"></i>&nbsp; Pay at the hotel</p>
+                    <a href="" class="float-right mr-5">Reviews &nbsp;<i class="fa fa-angle-right"></i></a>
                 </div>
             </div>
-    
         </div>
+       
     </div>
-
-
 @endsection
--- resources/views/hotel/room.blade.php
+++ resources/views/hotel/room.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 
+@section('style')
+<style>
+    .container-scroll{
+        background-color:white;
+    }
+</style>
+@endsection 
+
 @section('slide')
     <div class="li-title ml-3">Hotel</div>
     <li>
@@ -28,118 +36,56 @@
 @endsection
 
 @section('content')
-    <h3>Room Service</h3>
-    <div class="container pl-4">
         <div class="row">
-            <div class="card-deck english">
+            <div class="card-deck english text-center">
                 <div class="card bg-default">
-                        <div id="demo" class="carousel slide" data-ride="carousel">
 
-                                <!-- Indicators -->
-                                <ul class="carousel-indicators">
-                                    <li data-target="#demo" data-slide-to="0" class="active"></li>
-                                    <li data-target="#demo" data-slide-to="1"></li>
-                                    <li data-target="#demo" data-slide-to="2"></li>
-                                </ul>
-
-                                <!-- The slideshow -->
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <img class="card-img-top " src="{{asset('img/hotel/hotel1.jpg')}}" 
-                                        width="200" height="200" alt="Card image cap">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img class="card-img-top " src="{{asset('img/hotel/hotel3.jpg')}}" 
-                                        width="200" height="200" alt="Card image cap">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img class="card-img-top " src="{{asset('img/hotel/hotel2.jpg')}}" 
-                                        width="200" height="200" alt="Card image cap">
-                                    </div>
-                                </div>
-
-                                <!-- Left and right controls -->
-                                <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                                    <span class="carousel-control-prev-icon"></span>
-                                </a>
-                                <a class="carousel-control-next" href="#demo" data-slide="next">
-                                    <span class="carousel-control-next-icon"></span>
-                                </a>
-                        </div>
-
-
-        
-                    <!-- <img class="card-img-top " src="{{asset('img/hotel/hotel1.jpg')}}" 
-                    width="200" height="200" alt="Card image cap"> -->
-                    <h5 class="p-1 m-2">Standard Double <i class="fa fa-angle-right float-right"></i></h5>
-                    <h6 class="p-1 m-2"><i class="fa fa-user-o "></i> Max 2 adults</h6><hr>
-                   <p class="text-center text-primary"><i class="fa fa-angle-down float-center" 
-                   data-toggle="collapse" data-target="#demo1"></i>  Show More Options </p>
-                        <div id="demo1" class="collapse">
-                            <div class="card-body text-left pt-0">
-                                    <p class="card-text text-success"><i class="fa fa-user "></i>&nbsp;&nbsp;  2<br>
-                                    <i class="fa fa-check "></i> &nbsp;&nbsp;  Breakfast<br>
-                                    <i class="fa fa-certificate"></i> &nbsp;&nbsp; Special detail<br>
-                                    <i class="fa fa-tags"></i> &nbsp;&nbsp; $3</p>
-                                    
-                                    </p>
-                            </div>
-                        </div>
-                </div>
-                <div class="card bg-default">
-                    <div id="mo" class="carousel slide" data-ride="carousel">
-
+                    <div id="demo" class="carousel slide" data-ride="carousel">
                         <!-- Indicators -->
                         <ul class="carousel-indicators">
-                            <li data-target="#mo" data-slide-to="0" class="active"></li>
-                            <li data-target="#mo" data-slide-to="1"></li>
-                            <li data-target="#mo" data-slide-to="2"></li>
+                            <li data-target="#demo" data-slide-to="0" class="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
                         </ul>
 
                         <!-- The slideshow -->
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                                <img class="card-img-top " src="{{asset('img/hotel/hotel2.jpg')}}" 
-                                width="200" height="200" alt="Card image cap">
-                            </div>
-                            <div class="carousel-item">
                                 <img class="card-img-top " src="{{asset('img/hotel/hotel1.jpg')}}" 
-                                width="200" height="200" alt="Card image cap">
+                                width="150" height="150" alt="Card image cap">
                             </div>
                             <div class="carousel-item">
                                 <img class="card-img-top " src="{{asset('img/hotel/hotel3.jpg')}}" 
-                                width="200" height="200" alt="Card image cap">
+                                width="150" height="150" alt="Card image cap">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="card-img-top " src="{{asset('img/hotel/hotel2.jpg')}}" 
+                                width="150" height="150" alt="Card image cap">
                             </div>
                         </div>
-
                         <!-- Left and right controls -->
-                        <a class="carousel-control-prev" href="#mo" data-slide="prev">
+                        <a class="carousel-control-prev" href="#demo" data-slide="prev">
                             <span class="carousel-control-prev-icon"></span>
                         </a>
-                        <a class="carousel-control-next" href="#mo" data-slide="next">
+                        <a class="carousel-control-next" href="#demo" data-slide="next">
                             <span class="carousel-control-next-icon"></span>
                         </a>
-                </div>
+                    </div>
 
-                    <!-- <img class="card-img-top " src="{{asset('img/hotel/hotel2.jpg')}}" 
-                    width="200" height="200" alt="Card image cap"> -->
-                    <h5 class="p-1 m-2">Superior Double Room <i class="fa fa-angle-right float-right"></i></h5>
-                    <h6 class="p-1 m-2"><i class="fa fa-user-o "></i> Max 2 adults,plus 1 on extra bed<br>2 children(0-5 years)stay FREE!</h6><hr>
-                   <p class="text-center text-primary"><i class="fa fa-angle-down float-center" 
-                   data-toggle="collapse" data-target="#demo2"></i>  Show More Options </p>
-                        <div id="demo2" class="collapse">
-                            <div class="card-body text-left pt-0 text-success">
-                                    <p class="card-text"><i class="fa fa-user fa-2x "></i>  2 &nbsp;&nbsp;<i class="fa fa-user "></i> 2<br>
-                                    <i class="fa fa-check "></i>  Breakfast<br>
-                                    <i class="fa fa-certificate"></i> &nbsp;&nbsp;Special detail<br>
-                                    <i class="fa fa-tags"></i> &nbsp;&nbsp;$3</p>
-                                    
-                                    </p>
-                            </div>
+                    <P class="p-1 m-2">Standard Double <i class="fa fa-angle-right float-right"></i></P>
+                    <P class="p-1 m-2"><i class="fa fa-user-o "></i> Max 2 adults</P><hr>
+                    <p class="text-center text-primary"><i class="fa fa-angle-down float-center" 
+                        data-toggle="collapse" data-target="#demo1"></i>  Show More Options 
+                    </p>
+                    <div id="demo1" class="collapse">
+                        <div class="card-body text-left pt-0">
+                            <p class="card-text text-success"><i class="fa fa-user "></i>&nbsp;&nbsp;2<P>
+                            <p class="card-text text-success"><i class="fa fa-check "></i> &nbsp;&nbsp;Breakfast<P>
+                            <p class="card-text text-success"><i class="fa fa-certificate"></i> &nbsp;&nbsp;Special detail<P>
+                            <p class="card-text text-success"><i class="fa fa-tags"></i>&nbsp;&nbsp; $3</p>
                         </div>
-                </div> 
-                
-            </div>
+                    </div>
+                </div>
+            </div>  
         </div>
-    </div>
 @endsection
