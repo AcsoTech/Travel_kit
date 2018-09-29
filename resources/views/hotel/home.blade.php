3ce8d781117b4c3bd19d8c5469d2e82f3281af20
AcsoTech <acsotech.mm@gmail.com>
2018-09-30 04:01:42 +0630

    Filter

--- resources/views/hotel/home.blade.php
+++ resources/views/hotel/home.blade.php
@@ -61,7 +61,7 @@
                  <a href="{{ url('/review') }}" class="float-left card-link">
                     4 Like &nbsp;<i class="fa fa-heart-o"></i>
                 </a>
-                  <a href="#" class="float-right card-link" data-toggle="modal" data-target="#myModal">
+                  <a href="#" class="float-right card-link" data-toggle="modal" data-target="#review">
                     6 Reviews &nbsp;<i class="fa fa-commenting-o"></i>
                 </a>
             </div>
@@ -92,7 +92,7 @@
                  <a href="{{ url('/review') }}" class="float-left card-link">
                     4 Like &nbsp;<i class="fa fa-heart-o"></i>
                 </a>
-                <a href="#" class="float-right card-link" data-toggle="modal" data-target="#myModal">
+                <a href="#" class="float-right card-link" data-toggle="modal" data-target="#review">
                     6 Reviews &nbsp;<i class="fa fa-commenting-o"></i>
                 </a>
             </div>
@@ -123,14 +123,21 @@
                 <a href="{{ url('/review') }}" class="float-left card-link">
                     4 Like &nbsp;<i class="fa fa-heart-o"></i>
                 </a>
-                <a href="#" class="float-right card-link" data-toggle="modal" data-target="#myModal">
+                <a href="#" class="float-right card-link" data-toggle="modal" data-target="#review">
                     6 Reviews &nbsp;<i class="fa fa-commenting-o"></i>
                 </a>
             </div>
         </div>
     </div>
-    <div class="modal fade" id="myModal">
-        <div class="modal-dialog model-lg">
+
+    <div class="fixed_button mb-1">
+        <a href="#" class="btn btn-info btn-sm" data-toggle="modal" data-target="#filter">
+            <i class="fa fa-balance-scale"></i> Filter
+        </a>
+    </div>
+
+    <div class="modal fade" id="review">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <!-- Modal Header -->
                 <div class="modal-header">
@@ -157,7 +164,7 @@
                                 </p>
                             </div>
                         </div>
-                         <div class="card bg-light col">  
+                        <div class="card bg-light col">  
                             <div class="card-body">
                                 <p class="text-dark text-uppercase card-title font-weight-bold">
                                 Mr.Aung Nyi Thit
@@ -173,7 +180,7 @@
                                 </p>
                             </div>
                         </div>
-                         <div class="card bg-light col">  
+                        <div class="card bg-light col">  
                             <div class="card-body">
                                 <p class="text-dark text-uppercase card-title font-weight-bold">
                                 Mr.Sai Yan Naing 
@@ -189,7 +196,7 @@
                                 </p>
                             </div>
                         </div>
-                         <div class="card bg-light col">  
+                        <div class="card bg-light col">  
                             <div class="card-body">
                                 <p class="text-dark text-uppercase card-title font-weight-bold">
                                 Mr.Htet Htet Kyaw 
@@ -205,7 +212,7 @@
                                 </p>
                             </div>
                         </div>
-                         <div class="card bg-light col">  
+                        <div class="card bg-light col">  
                             <div class="card-body">
                                 <p class="text-dark text-uppercase card-title font-weight-bold">
                                 Mr.Nay Chi Thit
@@ -225,7 +232,119 @@
                 </div>
                 <!-- Modal footer -->
                 <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <input class="form-control" type="text"  placeholder="Write Something ..." 
+                    data-toggle="modal" data-target="#comment">
+                    <!-- <button class="btn btn-success mr-5" data-toggle="modal" data-target="#comment">Comment</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="comment">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title text-muted">Comment</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                
+                  
+                <div class="modal-body">
+                    <form action="" class="english bg-white">
+                        <div class="form-group">
+                        <label for="address">Address:</label>
+                        <input type="text" class="form-control" id="address" placeholder="Enter address" name="email">
+                        </div>
+                        <div class="form-group">
+                        <label for="comment">Comment:</label>
+                        <textarea class="form-control" rows="8" id="comment" placeholder="Write Something ..."></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-block btn-primary">Post</button>
+                    </form>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" type="submit" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+        
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="filter">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header">
+                <p class="modal-title card-title">Sort and Filter</p>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <div class="model-scroll" id="style-1">
+                        <p class="text-light bg-info">Sort by: <i class="fa fa-sort"></i></p>
+                        <div class="form-group">
+                            <select class="form-control" id="sel1" name="sellist1">
+                                <option>Price (Low to Hight)</option>
+                                <option>Price (Height to Low)</option>
+                                <option>Star (0 to 5)</option>
+                                <option>Star (5 to 0)</option>
+                            </select>
+                        </div>
+                        <br>
+                        <p class="text-light bg-info">Filter by: <i class="fa fa-balance-scale"></i></p>
+                        <h6>Star Rating</h6>
+                        <div class="form-check-inline">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="optradio">1
+                                <i class="fa fa-star text-warning"></i>
+                            </label>
+                        </div>
+                        <div class="form-check-inline">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="optradio">2 
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                            </label>
+                        </div>
+                        <div class="form-check-inline">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="optradio" disabled>3
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                            </label>
+                        </div>
+                        <div class="form-check-inline">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="optradio" disabled>4
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                            </label>
+                        </div>
+                        <div class="form-check-inline">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="optradio" disabled>5
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                                <i class="fa fa-star text-warning"></i>
+                            </label>
+                        </div>
+                        <br><br>
+                        <h6>Price</h6>
+                        <div class="form-group">
+                            <input type="text" name="" id="" class="form-control" placeholder="Almost Price">
+                        </div>
+
+                </div>
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Search</button>
                 </div>
             </div>
         </div>
